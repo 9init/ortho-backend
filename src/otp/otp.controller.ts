c100a0cd46473c53c231f622a14982179ad4ba49
Ahmed Hesham <emovent2e@gmail.com>
2024-05-09 06:34:44 +0300

    otp

--- src/otp/otp.controller.ts
+++ src/otp/otp.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseGuards, Param } from "@nestjs/common";
+import { Controller, Post, Body, UseGuards } from "@nestjs/common";
 import { OtpService } from "./otp.service";
 import { AuthGuard } from "src/auth/auth.guard";
 import { LoggedInUser } from "src/user/logged-in-user.decorator";
