bfb1f1841a1f3ad3fb7a808c1c4a50fc5cd1edd3
Ahmed Hesham <emovent2e@gmail.com>
2024-05-10 01:05:22 +0300

    pwd

--- src/decorators/hash-password/hash-password.decorator.ts
+++ src/decorators/hash-password/hash-password.decorator.ts
@@ -6,11 +6,6 @@ import { hashPasswordSync } from "./helper";
  * @returns The transformed hashed password value.
  */
 export const HashPassword = () =>
-  // Memoization ensures that the password is hashed only once and subsequent calls return the same hashed value.
-  (() => {
-    let hashed = undefined;
-    return Transform(({ value }: TransformFnParams) => {
-      hashed ??= hashPasswordSync(value);
-      return hashed;
-    });
-  })();
+  Transform(({ value }: TransformFnParams) => {
+    return hashPasswordSync(value);
+  });
